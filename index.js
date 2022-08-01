89e75685c6b3fe4746a5ba8194cb23d431b488e3
Murl Trainham <murltrainham@gmail.com>
2022-07-31 20:25:31 -0700

    Push

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const inquirer = require("inquirer");
 
+/*
+Functional Jest test from: https://jestjs.io/docs/getting-started
+This exports sum which is then called @ sum.test.js by means of _ const sum = require('../index'); _
+
 function sum(a, b) {
     return a + b;
   }
   module.exports = sum;
+  */
